6f1864faaa0b37cadc3636c7d27d000b67a506ac
VishalThapaliya <vishal.thapaliya@gmail.com>
2018-11-26 15:30:22 +0100

    brand list view and Cest parti page added

--- frontend/init/bootstrap/theme/theme.js
+++ frontend/init/bootstrap/theme/theme.js
@@ -54,7 +54,7 @@ import "./js/front.min.js"
 import "./js/components/hs.slick-carousel.js"
 
 
-//import "./vendor/jquery-migrate/dist/jquery-migrate.min.js"
+import "./vendor/jquery-migrate/dist/jquery-migrate.min.js"
 
 
 import "./vendor/appear.js"
